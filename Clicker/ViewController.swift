6f2134a6eb6f261671695a0fa015c8c86a44cd19
Jacob Finn <jacobfinn@macbook-pro-9.local>
2018-08-23 10:15:49 -0500

    Initial Commit

---
+++ Clicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Clicker
+//
+//  Created by Jacob Finn on 8/23/18.
+//  Copyright © 2018 Jacob Finn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
